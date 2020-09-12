17758231b63550a48d0105931f726ca8ce421890
sugijotaro <sugi.jotaro@gmail.com>
2020-09-12 20:57:32 +0900

    記事通りに実装してみた

--- kirikae_test/AppDelegate.swift
+++ kirikae_test/AppDelegate.swift
@@ -11,11 +11,57 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    /// 端末の種類
+    static var deviceType = 0
+    /// 端末の種類(iPhone、iPad)
+    enum DeviceType: Int {
+        case phone = 0,
+        pad = 1
+    }
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if AppDelegate.deviceType == DeviceType.pad.rawValue {
+            print("iPadだよ！")
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            let storyboard = UIStoryboard(name: "Tablet", bundle: nil)
+            print(storyboard)
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "rootView")
+            print(initialViewController)
+            self.window?.rootViewController = initialViewController
+            print(self.window?.rootViewController)
+            // iPad用のstoryboardを使用
+            self.window?.makeKeyAndVisible()
+        } else {
+            print("iPhoneだよ！")
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            print(storyboard)
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "rootView")
+            print(initialViewController)
+            self.window?.rootViewController = initialViewController
+            print(self.window?.rootViewController)
+            // iPhone用のstoryboardを使用
+            self.window?.makeKeyAndVisible()
+        }
         return true
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        /// 端末の向きを指定
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            // iPhoneは縦向き固定
+            AppDelegate.deviceType = DeviceType.phone.rawValue
+            return UIInterfaceOrientationMask.portrait
+        case .pad:
+            // iPadは横向き固定
+            AppDelegate.deviceType = DeviceType.pad.rawValue
+            return UIInterfaceOrientationMask.landscape
+        default:
+            return UIInterfaceOrientationMask.portrait
+        }
     }
 
     // MARK: UISceneSession Lifecycle
