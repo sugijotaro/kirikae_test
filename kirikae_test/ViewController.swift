510f10f2db50774cf7dda49502b320dc2d4a0708
sugijotaro <sugi.jotaro@gmail.com>
2020-09-12 20:44:08 +0900

    Initial Commit

---
+++ kirikae_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kirikae_test
+//
+//  Created by JotaroSugiyama on 2020/09/12.
+//  Copyright © 2020 JotaroSugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
